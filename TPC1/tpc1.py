da606a6395e5275b3939dc20cb66ae4a6696e466
AdelioFernandes78778 <a78778@alunos.uminho.pt>
2024-02-23 12:47:25 +0000

    tpc1 100% funcional + resultado

--- TPC1/tpc1.py
+++ TPC1/tpc1.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 def processar_dados(filename):
-    with open(filename, 'r') as file:
+    with open(filename, 'r', encoding='utf-8') as file:
         linhas = file.readlines()
 
     linhas = linhas[1:]
@@ -10,44 +10,61 @@ def processar_dados(filename):
     modalidades = set()
     aptos = 0
     inaptos = 0
-    distribuicao_idades = {'[20-24]': 0, '[25-29]': 0, '[30-34]': 0, '[35-39]': 0, '[40-44]': 0, '[45-49]': 0}
-
+    distribuicao_idades = {'[20-24]': {'num_atletas': 0, 'percentagem': 0},
+                           '[25-29]': {'num_atletas': 0, 'percentagem': 0},
+                           '[30-34]': {'num_atletas': 0, 'percentagem': 0},
+                           '[35-39]': {'num_atletas': 0, 'percentagem': 0},
+                           '[40-44]': {'num_atletas': 0, 'percentagem': 0},
+                           '[45-49]': {'num_atletas': 0, 'percentagem': 0}}
 
     for linha in linhas:
-        colunas = linha.split('\t')
+        colunas = linha.split(',')
+
+        if len(colunas) < 13:  # Verifica se há dados suficientes na linha
+            continue  # Ignora a linha se não houver dados suficientes
 
         modalidade = colunas[8]
         idade = int(colunas[5])
 
         modalidades.add(modalidade)
 
-        federado = colunas[11]
-        resultado = colunas[12]
-        if federado == 'true' and resultado == 'true':
+        resultado = colunas[12].strip().lower()
+        if resultado == 'true':
             aptos += 1
         else:
             inaptos += 1
 
-        for faixa in distribuicao_idades:
+        for faixa, info in distribuicao_idades.items():
             faixa_inicio, faixa_fim = map(int, faixa.strip('[]').split('-'))
             if faixa_inicio <= idade <= faixa_fim:
-                distribuicao_idades[faixa] += 1
+                info['num_atletas'] += 1
                 break
 
     modalidades_ordenadas = sorted(list(modalidades))
 
     total_atletas = aptos + inaptos
-    percentagem_aptos = (aptos / total_atletas) * 100
-    percentagem_inaptos = (inaptos / total_atletas) * 100
+    for faixa, info in distribuicao_idades.items():
+        info['percentagem'] = round((info['num_atletas'] / total_atletas) * 100, 2)
+
+    percentagem_aptos = (aptos / total_atletas) * 100 if total_atletas != 0 else 0
+    percentagem_inaptos = (inaptos / total_atletas) * 100 if total_atletas != 0 else 0
 
     return modalidades_ordenadas, percentagem_aptos, percentagem_inaptos, distribuicao_idades
 
 
-modalidades, percentagem_aptos, percentagem_inaptos, distribuicao_idades = processar_dados("./emd.csv")
+# Obtém o diretório do script atual
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+# Concatena o diretório do script com o nome do arquivo
+file_path = os.path.join(dir_path, "emd.csv")
+
+# Chama a função processar_dados usando o caminho absoluto do arquivo
+modalidades, percentagem_aptos, percentagem_inaptos, distribuicao_idades = processar_dados(file_path)
 
 print("Modalidades:", modalidades)
 print("Atletas aptos:", percentagem_aptos)
 print("Atletas inaptos:", percentagem_inaptos)
 print("Atletas por escalão etário:")
-for faixa_etaria, num_atletas in distribuicao_idades.items():
-    print(faixa_etaria, ":", num_atletas)
+for faixa_etaria, info in distribuicao_idades.items():
+    print(f"{faixa_etaria} : {info['num_atletas']} ({info['percentagem']}%)")
+
